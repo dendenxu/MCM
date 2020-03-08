247ee8d804903bad3046ec58f378f7e5cb0a32af
xudenden <56295612@qq.com>
2020-03-08 13:37:48 +0800

    before applying osmosis

--- cell.py
+++ cell.py
@@ -12,32 +12,51 @@ class SandCastleSimulator2D:
         self.depth = depth
         # The front of our sand castle
         # self.edge = np.random.normal(width / 2, width / 4, depth).round(0)
-        self.edge = width - (np.cos(np.linspace(0, np.pi / 2, width)) * depth).round(0)
-        self.edge = np.asarray([0 if x < 0 else (width - 1 if x > width - 1 else x) for x in self.edge], dtype=int)
+        # self.edge = depth - ((np.cos(np.linspace(-np.pi, np.pi, width)) + 1) * depth / 2).round(0)
+        self.edge = depth - ((np.cos(np.linspace(-np.pi * 3, np.pi * 3, width)) + 1) * depth / 10 + depth / 2).round(0)
+        # self.edge = depth - (np.cos(np.linspace(0, np.pi / 2, width)) * depth).round(0)
+        # self.edge = depth - ((np.sqrt(1 - np.linspace(-1, 1, width) ** 2)) * depth / 2).round(0)
+        self.edge = np.asarray([0 if x < 0 else (depth - 1 if x > depth - 1 else x) for x in self.edge], dtype=int)
         # self.edge.sort()
         self.life = np.array([[i >= self.edge[j] for j in range(width)] for i in range(depth)])
-        self.humidity = np.full((width, depth), fill_value=0.0)
+        self.humidity = np.full((depth, width), fill_value=0.1)
         # self.humidity[0:int(width / 2), :] = np.full((int(width / 2), depth), fill_value=0.5)
-        self.drop_dead()
-        self.slope = self.calculate_slope()
+        self.slope = np.zeros((width,), float)
+        self.update_slope()
         sea_blue = np.array([0, 102, 153, 256]) / 256
         sandy_brown = np.array([244, 164, 96, 256]) / 256
         self.sea_sand = mpl.colors.ListedColormap(np.concatenate((np.tile(sea_blue, 128).reshape(128, 4),
                                                                   np.tile(sandy_brown, 128).reshape(128, 4)), axis=0))
         hot = plt.cm.get_cmap('Oranges', 256)
         sea_wet_sand = hot(np.linspace(0, 1, 256))
-        sea_wet_sand[0:160] = sea_wet_sand[256 - 160::]
-        sea_wet_sand[160::] = np.tile(sea_blue, 256 - 160).reshape(256 - 160, 4)
+        sea_wet_sand[0:128] = sea_wet_sand[128::]
+        sea_wet_sand[128::] = np.tile(sea_blue, 128).reshape(128, 4)
         self.sea_wet_sand = mpl.colors.ListedColormap(sea_wet_sand)
 
-        self.shear_rate = 1.5
-        self.humidify_rate = 0.01
+        self.shear_rate = 0.5
+        self.humidify_rate = 0.005
+        self.humidify_depth = 1
+        self.osmotic_rate = 0.002
+        self.osmotic_depth = 5
+        # self.osmosis()
+        self.drop_dead()
 
-        self.delta_humidity = np.flip(np.exp(np.linspace(-10, 0, self.depth))) * self.humidify_rate
-        self.delta_humidity = np.tile(self.delta_humidity, self.width).reshape(self.depth, self.width)
+        self.delta_humidity = np.flip(
+            np.exp(np.linspace(-10 * self.humidify_depth, 0, self.depth))) * self.humidify_rate
+        self.delta_humidity = np.tile(self.delta_humidity, self.width).reshape(self.width, self.depth)
         self.delta_humidity = np.transpose(self.delta_humidity)
 
-    def calculate_slope(self):
+    def osmosis(self):
+        osmotic_depth = self.delta * self.osmotic_depth
+        longest = np.sqrt(self.width ** 2 + self.depth ** 2)
+        for i in range(self.width):
+            for j in range(self.edge[i] - osmotic_depth,
+                           self.edge[i] - osmotic_depth):
+                for k in range(i - osmotic_depth, i + osmotic_depth):
+                    distance = np.sqrt((j - self.edge[i]) ** 2 + (k - i) ** 2)
+                    self.humidity[j, k] += np.exp(-10 * distance / longest) * self.osmotic_rate
+
+    def update_slope(self):
         slope = np.asarray([(self.edge[i + self.delta] - self.edge[i - self.delta]) / (2 * self.delta)
                             for i in range(self.delta, self.width - self.delta)])
         # for d in range(1, self.delta):
@@ -46,28 +65,36 @@ class SandCastleSimulator2D:
         #                 for i in range(d, self.width - self.delta * 2 - d)])
         slope = np.insert(slope, 0, np.full([self.delta, ], slope[0]))
         slope = np.append(slope, np.full([self.delta, ], slope[-1]))
-        return np.arctan(slope)
+        self.slope = np.arctan(abs(slope))
 
     def update_life(self):
         self.life = np.array([[i >= self.edge[j] for j in range(self.width)] for i in range(self.depth)])
 
     def drop_dead(self):
         self.humidity = np.array(
-            [[self.humidity[i, j] if self.life[i, j] else 1. for j in range(self.width)] for i in range(self.depth)])
+            [[self.humidity[i, j] if self.life[i, j] else 1.5 for j in range(self.width)] for i in range(self.depth)])
 
-    def bye_prick(self):
-        for i in range(self.delta, self.width - self.delta):
-            if self.edge[i] < min(self.edge[i - self.delta:i + self.delta]):
-                self.edge[i] = max(self.edge[i - self.delta], self.edge[i + self.delta])
+    def drop_prick(self):
+        delta = self.delta * 2
+        edge = self.edge
+        for i in range(delta, self.width - delta):
+            neighbors = np.concatenate([self.edge[i - delta:i], self.edge[i + 1:i + delta + 1]])
+            # print("Current edge[i] is {}".format(edge[i]))
+            # print("Current neighbors are {}".format(neighbors))
+            if edge[i] < min(neighbors):
+                edge[i] = max(neighbors)
+        self.edge = edge
         # self.life = np.array([[i >= self.edge[j] for j in range(self.width)] for i in range(self.depth)])
 
     def wave(self):
-        # Let's assume the slope and humidity level and the angle of the impact will change what our sand castle look like
+        # Let's assume the slope and humidity level and the angle of the
+        # impact will change what our sand castle look like
 
         cosine = abs(np.cos(self.slope))
         sine = abs(np.sin(self.slope))
 
-        # Then, according to the humidity level and angle of impact, we'll determine the san cell that is currently alive
+        # Then, according to the humidity level and angle of impact,
+        # we'll determine the san cell that is currently alive
         delta_shear = sine * self.shear_rate
         for k in range(self.delta):
             for i in range(self.width):
@@ -77,20 +104,20 @@ class SandCastleSimulator2D:
                     # self.life[self.edge[i], i] = False
                     self.edge[i] += 1 if self.edge[i] < self.depth - 1 else 0
                     delta_shear[i] -= delta_shear[i] / self.delta
+            self.drop_prick()
 
-        self.bye_prick()
         # We'll firstly update the humidity level according to the angle of impact
-        self.delta_humidity *= cosine
+        delta_humidity = self.delta_humidity * cosine
         # print(delta_humidity.shape)
         # print(self.humidity.shape)
         for i in range(self.width):
             # print("Current i value is {}, and current edge value is {}".format(i, self.edge[i]))
             # print("Humidity from {} to {} is changed by {} to {} of delta humidity"
             #       .format(self.edge[i], self.depth, 0, self.depth - self.edge[i] - 1))
-            self.humidity[self.edge[i]::, i] += self.delta_humidity[0:self.depth - self.edge[i], i]
-
+            self.humidity[self.edge[i]::, i] += delta_humidity[0:self.depth - self.edge[i], i]
+        # self.osmosis()
         # Finally, we'll update the sand castle's humidity information and slope information
-        self.slope = self.calculate_slope()
+        self.update_slope()
         # self.drop_dead()
 
 
@@ -99,14 +126,18 @@ class SandCastleSimulator2D:
 # from cell import *
 # delta should be no smaller than 1
 sim = SandCastleSimulator2D(1000, 1000, 5)
-plt.figure()
+fig = plt.figure()
+fig.add_subplot(231)
 plt.imshow(sim.humidity, cmap=sim.sea_wet_sand)
-plt.figure()
-plt.imshow(sim.life, cmap=sim.sea_sand)
+# plt.figure()
+# plt.imshow(sim.life, cmap=sim.sea_sand)
 sim.wave()
-for _ in range(100):
-    sim.wave()
-sim.update_life()
-sim.drop_dead()
-plt.figure()
-plt.imshow(sim.humidity, cmap=sim.sea_wet_sand)
+for i in range(5):
+    for _ in range(100):
+        sim.wave()
+    sim.update_life()
+    sim.drop_dead()
+    fig.add_subplot(230 + i + 2)
+    plt.imshow(sim.humidity, cmap=sim.sea_wet_sand)
+    # plt.figure()
+    # plt.imshow(sim.life, cmap=sim.sea_sand)
